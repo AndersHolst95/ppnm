a255eaa19b74d933fad8c841ad6439659de72ae0
Anders Holst Rasmussen <201606907@post.au.dk>
2020-03-06 15:56:30 +0100

    ln(gamma) added

--- gnuplot/main-lngamma.cs
+++ gnuplot/main-lngamma.cs
@@ -5,7 +5,7 @@ class mainLnGamma{
 	public static void Main(){
 		double eps = 1.0/32;
 		double dx = 1.0/16;		
-		for(double x = 0 + eps; x<30; x += dx){
+		for(double x = 0 + eps; x<7; x += dx){
 			WriteLine($"{x,10:f3} {math.lngamma(x), 15:f8}");
 		}
 	}
